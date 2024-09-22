0ba846480ecbdefc9621da496474b7b02041bf81
Mahe2patel <patelmahetab363@gmail.com>
2024-09-23 00:39:32 +0530

    some changes

---
+++ 26 th/scripts.js
@@ -0,0 +1,6 @@
+// console.log('hii')
+const likeButton=document.querySelector('#heart');
+likeButton.addEventListener('dblclick',function(){
+      likeButton.style.background='red';
+    // likeButton.style.color='red';
+})
